45b9a8d6480671312fde9e09d19c1ffa884ae487
Marcel Birkner <marcel_birkner-extern@gothaer-systems.de>
2016-04-11 14:40:46 +0200

    JIRA-123 erster Controller angelegt

---
+++ src/main/java/de/wdr/demo/HelloWorldController.java
@@ -0,0 +1,21 @@
+package de.wdr.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home(@RequestParam String name) {
+		return "Hello World! How are you today " + name;
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(HelloWorldController.class, args);
+	}
+}
